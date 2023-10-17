2be4d93f9c0af1895c698e97ac33a772f1ac014b
andybar <ab@webmechanica.ru>
2023-10-17 15:56:21 +0300

    change code

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
+let days = document.querySelectorAll('.days');
+let tax = document.querySelectorAll('.tax');
+let taxSum = document.querySelectorAll('.tax_sum');
+let vacation = document.querySelectorAll('.vacation');
+let salaryWhole = document.querySelectorAll('.salary_whole');
+let salaryProg = document.querySelectorAll('.salary_prog');
+let salaryContent = document.querySelectorAll('.salary_content');
+let salaryElem = document.querySelectorAll('.salary_elem');
+
 let itemHidden = document.querySelector('.hidden');
 let itemDetailed = document.querySelector('.detailed');
-let betContentSum = document.querySelectorAll('.bet_content-sum');
 let depositSum = document.querySelector('.deposit');
 let quantityMoney = document.querySelector('.quantity_money');
-
-let days = document.querySelectorAll('.days');
-let daysText = document.querySelectorAll('.days_text');
-let hoursProg = document.querySelectorAll('.hours');
-
 let form = document.querySelector('form');
 let formControl = document.querySelector('.form_control');
 let formVacation = document.querySelector('.form_vacation');
@@ -16,53 +19,55 @@ let formSalaryDefault = document.querySelector('.form_salary-default');
 let formDetailed = document.querySelector('.form_detailed');
 let inputBtnVacation = document.querySelector('.input_btn-vacation');
 let inputBtn = document.querySelector('.input_btn');
-
-let tax = document.querySelectorAll('.tax');
-let taxSum = document.querySelectorAll('.tax_sum');
-
-let vacation = document.querySelectorAll('.vacation');
-let vacationMoney = document.querySelectorAll('.vacation_money');
-let vacationDays = document.querySelectorAll('.vacation_days');
-
-let salary = document.querySelectorAll('.salary');
-let salaryWhole = document.querySelectorAll('.salary_whole');
-let salaryProg = document.querySelectorAll('.salary_prog');
-let salaryContent = document.querySelectorAll('.salary_content');
-let salaryProgContent = document.querySelectorAll('.salary_prog_content');
-
-let quantityVacation = document.querySelector('.quantity_vacations').value;
-quantityVacation = parseInt(quantityVacation);
 let quantityVacationItem = document.querySelector('.quantity_vacations');
 let vacationNum = document.querySelector('.num_vacations');
-
-let salaryElem = document.querySelectorAll('.salary_elem');
 let monthlySalary = document.querySelector('.monthly_salary'); 
 let salaries = document.querySelector('.salaries'); 
 
-let arrSalaryNums = [];
-let arrSalary = [];
 let sumSalary;
 let vacationCount;
+
+let arrSalaryNums = [];
+let arrSalary = [];
 
 let date = new Date();
 let year = date.getFullYear();
 let month = date.getMonth();
 let arrDate = [month + 1, year].join('.');
 
-monthlySalary.addEventListener('click', (e) => {
-  arrSalary = JSON.parse(localStorage.getItem("Годовая зарплата"));
+let quantityVacation = document.querySelector('.quantity_vacations').value;
+quantityVacation = parseInt(quantityVacation);
 
-  e.preventDefault();
-  salaryElem.forEach(function(n, i) {
-    n.innerHTML = arrSalary[i].join(': ') + ' ₽';
+function clickBtn(item, show, style) {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    show.classList.toggle(style);
+    form.classList.toggle('shift');
+    return false;
   });
+}
 
+function foreachData(item, input) {
+  item.forEach((i) => {
+    i.innerHTML = input;
+  });
+}
+
+function replacingElem(item) {
+  arrSalary[11][1] = item;
+  arrSalary[11].pop();
+  arrSalary[11].push(Math.round(item));
+}
+
+monthlySalary.addEventListener('click', (e) => {  
+  e.preventDefault();
+  arrSalary = JSON.parse(localStorage.getItem("Годовая зарплата"));
+  salaryElem.forEach((n, i) => n.innerHTML = arrSalary[i].join(': ') + ' ₽');
   return false;
 });
 
 formSalaryDefault.addEventListener('click', (e) => {
   e.preventDefault();
-  
   arrSalary = [
     ['Октябрь', 15582],
     ['Ноябрь', 48200],
@@ -79,7 +84,6 @@ formSalaryDefault.addEventListener('click', (e) => {
   ];
   
   localStorage.setItem("Годовая зарплата", JSON.stringify(arrSalary));
-  
   formSalaryDefault.disabled = true;
   return false;
 });
@@ -120,153 +124,110 @@ inputBtnVacation.addEventListener('click', (e) => { // вывести колич
 });
 //
 
-function clickBtn(item, show, style) {
-  item.addEventListener('click', (e) => {
-    e.preventDefault();
-    show.classList.toggle(style);
-    form.classList.toggle('shift');
-    return false;
-  });
-}
-
 clickBtn(formVacation, itemHidden, 'show-hidden');
 
+let flagControl = false; 
 formControl.addEventListener('click', (e) => { // кнопка посчитать
-  e.preventDefault();
-  let betProg = 325;
+  if (flagControl == false) {
+    e.preventDefault();
+    let betProg = 325;
 
-  let quantityDays = document.querySelector('.quantity_days').value;
-  quantityDays = parseInt(quantityDays);
+    let quantityDays = document.querySelector('.quantity_days').value;
+    quantityDays = parseInt(quantityDays);
 
-  let timeProg = document.querySelector('.time_prog').value;
-  timeProg = parseInt(timeProg);
+    let timeProg = document.querySelector('.time_prog').value;
+    timeProg = parseInt(timeProg);
 
-  let taxMoney = document.querySelector('.tax_money').value;
-  taxMoney = parseInt(taxMoney);
+    let taxMoney = document.querySelector('.tax_money').value;
+    taxMoney = parseInt(taxMoney);
 
-  let salarySum;
-  let salaryVacationTaxSum;
-  let salarySumAndTax;
-  let deposit;
-  let vacationSum;
+    let salarySum;
+    let salaryVacationTaxSum;
+    let salarySumAndTax;
+    let deposit;
+    let vacationSum;
 
-  quantityVacation = quantityVacationItem.value;
+    quantityVacation = quantityVacationItem.value;
 
-  if (quantityDays && timeProg && taxMoney) {
-    let taxSumHalf = (taxMoney / 3) / 2;
-    let betContent = 30000 / (quantityDays * 8);
-    let salaryProgSum = betProg * timeProg;
-    let salaryContentSum = (quantityDays * 8 - timeProg) * betContent;
+    if (quantityDays && timeProg && taxMoney) {
+      let taxSumHalf = (taxMoney / 3) / 2;
+      let betContent = 30000 / (quantityDays * 8);
+      let salaryProgSum = betProg * timeProg;
+      let salaryContentSum = (quantityDays * 8 - timeProg) * betContent;
 
-    if(quantityMoney.value && quantityVacation) {
-      vacationSum = ((quantityMoney.value / 12) / 20.5) * quantityVacation;
-      salaryVacationTaxSum = (betProg * timeProg) + ((quantityDays * 8 - timeProg) * betContent) + vacationSum + taxSumHalf;
-    } else {
-      salarySumAndTax = (betProg * timeProg) + ((quantityDays * 8 - timeProg) * betContent) + taxSumHalf;
-    }
-
-    if(quantityMoney.value && quantityVacation) {
-      deposit = salaryVacationTaxSum / 10;
-    } else {
-      deposit = salarySumAndTax / 10;
-    }
-
-    salarySum = (betProg * timeProg) + ((quantityDays * 8 - timeProg) * betContent);
-
-    depositSum.innerHTML = Math.round(deposit);
-
-    function foreachData(item, input) {
-      item.forEach((i) => {
-        i.innerHTML = input;
-      });
-    }
-
-    let numDay = quantityVacation % 10;
-    let daysDeclination;
-
-    if(quantityVacation > 4 && quantityVacation < 20 || numDay == 0) {
-      daysDeclination = "дней";
-    } else if(numDay > 1 && numDay < 5) {
-      daysDeclination = "дня";
-    } else if(quantityVacation == 1) {
-      daysDeclination = "день";
-    }
-
-    if(quantityMoney.value && quantityVacation) {
-      foreachData(salaryWhole, Math.round(salaryVacationTaxSum)); /* зп округлённое */
-    } else {
-      foreachData(salaryWhole, Math.round(salarySumAndTax)); /* зп округлённое */
-    }
-
-    //
-    let itemDetailedTextTax = "<span>Налоги:</span><br><b>"
-    + taxMoney + "</b> - сумма налогов за квартал<br>"
-    + "<b>" + taxMoney + " / 3) / 2 = " + taxSumHalf + "</b> - половина от налогов за месяц<br><br>";
-
-    let itemDetailedTextSalary = "ЗП (с контентными работами):<br>"
-    + "<b>" + quantityDays + "</b> - отработанные дни<br><br>"
-    + "<b>30000 / (" + quantityDays + " * 8) = " + betContent + "</b> - ставка контента за 1 час<br>"
-    + "<b>(" + quantityDays + " * 8 - " + timeProg + ") * " + betContent + " = " + salaryContentSum + "</b> - зп за контентые работы"
-    + "<p><b>"
-    + timeProg + " * 325 = " + salaryProgSum
-    + "</b> - зп за программерские работы </p>"
-    + "<p><b>"
-    + salaryProgSum + " + " + salaryContentSum + " = " + salarySum
-    + "</b></p>"
-    + "<p><b>";
-
-    itemDetailed.innerHTML += quantityMoney.value && quantityVacation ? itemDetailedTextTax
-      + "Отпускные:<br><b>((" + quantityMoney.value + " / 12) / 20,5) * " + quantityVacation + " = " + vacationSum + "</b> - отпускные за " + quantityVacation + " " + daysDeclination
-      + "<br>"
-      + "<br>"
-      + itemDetailedTextSalary
-      + salarySum + " + " + vacationSum + " + " + taxSumHalf + " = " + salaryVacationTaxSum
-      + "</b></p>" : itemDetailedTextTax
-      + itemDetailedTextSalary
-      + salarySum + " + " + taxSumHalf + " = " + salarySumAndTax
-      + "</b></p>";
-    //
-
-    if(quantityVacation !== '' || quantityVacation !== 0) {
-      localStorage.setItem("Количество отпускных", JSON.stringify(vacationNum.value));
-    }
-
-    function replacingElem(item) {
-      arrSalary[11][1] = item;
-      arrSalary[11].pop();
-      arrSalary[11].push(Math.round(item));
-    }
-
-    formSalary.addEventListener('click', (e) => {
-      e.preventDefault();
-
-      if(arrSalary[11][1] == Math.round(salaryVacationTaxSum) ||
-         arrSalary[11][1] == Math.round(salarySumAndTax)) {
-
-        alert("Вы уже добавили данные в таблицу");
-        return false;
+      if(quantityMoney.value && quantityVacation) {
+        vacationSum = ((quantityMoney.value / 12) / 20.5) * quantityVacation;
+        salaryVacationTaxSum = (betProg * timeProg) + ((quantityDays * 8 - timeProg) * betContent) + vacationSum + taxSumHalf;
       } else {
-
-        arrSalary.push(arrSalary.shift());
-
-        if(quantityMoney.value && quantityVacation) {
-          replacingElem(salaryVacationTaxSum);
-        } else {
-          replacingElem(salarySumAndTax);
-        }
-
-        localStorage.setItem("Годовая зарплата", JSON.stringify(arrSalary));
-
-        formSalary.disabled = true;
-        return false;
+        salarySumAndTax = (betProg * timeProg) + ((quantityDays * 8 - timeProg) * betContent) + taxSumHalf;
       }
-    });
 
-    clickBtn(formDetailed, itemDetailed, 'show');
-    clickBtn(formDetailed, salaries, 'show');
+      quantityMoney.value && quantityVacation ? deposit = salaryVacationTaxSum / 10 : deposit = salarySumAndTax / 10;
 
-  } else {
-    alert("Введите корректные данные");
-    return false;
+      salarySum = (betProg * timeProg) + ((quantityDays * 8 - timeProg) * betContent);
+
+      depositSum.innerHTML = Math.round(deposit);
+      
+      let numDay = quantityVacation % 10;
+      let daysDeclination;
+
+      quantityVacation > 4 && quantityVacation < 20 || numDay == 0 ? daysDeclination = "дней" :
+      numDay > 1 && numDay < 5 ? daysDeclination = "дня" :
+      quantityVacation == 1 ? daysDeclination = "день" : "";
+
+      quantityMoney.value && quantityVacation ? foreachData(salaryWhole, Math.round(salaryVacationTaxSum)) : foreachData(salaryWhole, Math.round(salarySumAndTax));
+
+      let itemDetailedTextTax = `
+        <span>Налоги:</span><br>
+        <b>${taxMoney}</b> - сумма налогов за квартал<br>
+        <b>${taxMoney} / 3) / 2 = ${taxSumHalf}</b> - половина от налогов за месяц<br><br>
+      `;
+
+      let itemDetailedTextSalary = `
+        ЗП (с контентными работами):<br>
+        <b>${quantityDays}</b> - отработанные дни<br><br>
+        <b>30000 / (${quantityDays} * 8) = ${betContent}</b> - ставка контента за 1 час<br>
+        <b>(${quantityDays} * 8 - ${timeProg}) * ${betContent} = ${salaryContentSum}</b> - зп за контентые работы
+        <p><b>${timeProg} * 325 = ${salaryProgSum}</b> - зп за программерские работы </p>
+        <p><b>${salaryProgSum} + ${salaryContentSum} = ${salarySum}</b></p>
+        <p><b>
+      `;
+
+      let ititemDetailedTextVacation = `
+        Отпускные:<br>
+        <b>((${quantityMoney.value} / 12) / 20,5) * ${quantityVacation} = ${vacationSum}</b> - отпускные за  ${quantityVacation + daysDeclination}<br><br>
+        ${itemDetailedTextSalary}
+        ${salarySum} + ${vacationSum} + ${taxSumHalf} = ${salaryVacationTaxSum}</b></p>
+      `;
+
+      itemDetailed.innerHTML += quantityMoney.value && quantityVacation ? itemDetailedTextTax + ititemDetailedTextVacation :
+      itemDetailedTextTax + itemDetailedTextSalary + salarySum + " + " + taxSumHalf + " = " + salarySumAndTax + "</b></p>";
+
+      if(quantityVacation !== '' || quantityVacation !== 0) localStorage.setItem("Количество отпускных", JSON.stringify(vacationNum.value));
+
+      formSalary.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if(arrSalary[11][1] == Math.round(salaryVacationTaxSum) ||
+          arrSalary[11][1] == Math.round(salarySumAndTax)) {
+          alert("Вы уже добавили данные в таблицу");
+          return false;
+        } else {
+          arrSalary.push(arrSalary.shift());
+          quantityMoney.value && quantityVacation ? replacingElem(salaryVacationTaxSum) : replacingElem(salarySumAndTax);
+          localStorage.setItem("Годовая зарплата", JSON.stringify(arrSalary));
+          formSalary.disabled = true;
+          return false;
+        }
+      });
+
+      clickBtn(formDetailed, itemDetailed, 'show');
+      clickBtn(formDetailed, salaries, 'show');
+
+    } else {
+      return false;
+    }
   }
+
+  flagControl = true;
 });
